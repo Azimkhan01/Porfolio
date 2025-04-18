a8cde9ac798d801927b9f3b43bba72343cdd3f94
Azimkhan01 <azimkarimrahim@gmail.com>
2025-04-18 20:41:42 +0530

    contact page and about page updated

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import ReactGA from 'react-ga4';
-// Initialize Google Analytics with your GA4 Measurement ID
-ReactGA.initialize('G-VDXCWBK4XB');
-
-// Track page views
-ReactGA.send('pageview'); // Sends a pageview hit when the app starts
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
